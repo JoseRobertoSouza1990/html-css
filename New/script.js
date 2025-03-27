e0b11fe41e670bee90363aeb06641eda1beea872
José Roberto de Souza <joserobertosouza405@gmail.com>
2025-03-27 15:58:11 -0300

    sistema

---
+++ New/script.js
@@ -0,0 +1,16 @@
+function toggleDropdown(id) {
+    document.getElementById(id).classList.toggle("show");
+}
+
+// Fechar o drop-down se o usuário clicar fora dele
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
